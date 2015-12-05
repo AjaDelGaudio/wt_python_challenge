829fac28be4c1a3cdd3b3a49110a157959b03312
wt-test <ajadelgaudio@gmail.com>
2015-12-05 11:50:52 -0500

    add views

--- conditions/views.py
+++ conditions/views.py
@@ -1,10 +1,14 @@
 
 # Create your views here.
 from rest_framework import viewsets
-from conditions.models import Condition
-from conditions.serializer import ConditionSerializer
+from conditions.models import Condition, Treatment
+from conditions.serializer import ConditionSerializer, TreatmentSerializer
 
 
 class ConditionViewSet(viewsets.ModelViewSet):
     queryset = Condition.objects.all()
     serializer_class = ConditionSerializer
+
+class TreatmentViewSet(viewsets.ModelViewSet):
+    queryset = Treatment.objects.all()
+    serializer_class = TreatmentSerializer
