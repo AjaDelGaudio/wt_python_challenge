4978edf57a571a19322f06bd4e53f1ba88fa7e73
wt-test <ajadelgaudio@gmail.com>
2015-12-08 00:04:30 -0500

    resolve conditions.Treatment serializer condition definition

--- conditions/serializer.py
+++ conditions/serializer.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from conditions.serializer import serializers
 
+class ConditionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = 'conditions.Condition'
+        fields = ('id', 'name',)
+
+    def create(self, validated_data):
+        return Condition.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
 class TreatmentSerializer(serializers.HyperlinkedModelSerializer):
+    condition = ConditionSerializer(serializers.HyperlinkedRelatedField)
     class Meta:
         model = 'conditions.Treatment'
         fields = ('id', 'condition', 'treatment_name', 'description', 'display_name',)
@@ -14,20 +29,5 @@ class TreatmentSerializer(serializers.HyperlinkedModelSerializer):
         instance.treatment_name = validated_data.get('treatment_name', instance.treatment_name)
         instance.description = validated_data.get('description', instance.description)
         instance.display_name = validated_data.get('display_name', instance.display_name)
-        instance.save()
-        return instance
-
-
-class ConditionSerializer(serializers.HyperlinkedModelSerializer):
-    treatments = TreatmentSerializer()
-    class Meta:
-        model = 'conditions.Treatment'
-        fields = ('id', 'name', 'treatments',)
-
-    def create(self, validated_data):
-        return Condition.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
         instance.save()
         return instance
